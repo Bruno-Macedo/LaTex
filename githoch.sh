ee809229efcb5b5eb9a6c067adf412814e99ab0f
Bruno-Macedo <bruno.macedoxxi@hotmail.com>
2021-10-01 19:38:10 +0200

    -- Fri Oct  1 19:38:10 2021

---
+++ githoch.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Mit diesem Skript laede ich Datei hoch (add ., commit und push)
+
+usage () {
+    echo "Nutzung: ${0} [VERZEICHNIS] '[COMMIT-text]' [REPOSITORY] [BRANCHE]" >&2
+    echo "Beispiel: githoch . 'Das ist ein Commit' origin branche-name" >&2
+    echo 'Falls [REPOSITORY] und [BRANCHE] leer sind, dann wird zum origin gepushed' >&2
+    exit 1
+}
+
+
+fehler () {
+    local MESSAGE="${@}"
+    if [[ "${?}" -ne 0 ]]
+    then
+	echo "${@}" >&2
+	exit 1
+    fi
+}
+git add "${1}"
+fehler "Das Verzeichnis konnte nicht hochgeladen werden."
+
+DATUM=$(date +%c)
+git commit -m "${2} -- ${DATUM} "
+fehler "Der Commit ist fehlgeschlagen"
+
+git push "${3}" "${4}"
+fehler "Es gibt ein Problem mit dem Brachen"
+
+#for NAME in "${@}"
+#do
+#    echo "${NAME}"
+   
+#done
+
+#"VERZEICHNIS="${1}"
+
+# git add
+#if [[ "${1}" == "" ]]
+#then
+#    git add .
+#else
+#    git add "${VERZEICHNIS}"
+#fi
+
+#shift
+
+# git commit
+#if [[ "${1}" == "" ]]
+#then
+#    echo "Schreiben Sie bitte eine Nachricht fuer Ihren commit."
+#else
+#	DATE_TIME=$(date +%c)
+#    git commit - "${1} -- ${DATE_TIME}"
+#fi
+
+#shift
+
+# git push
+#ORIGIN="${1}"
+#BRANCH="${2}"
+
+#if [[ "${1}" == "" ]]
+#then
+#    git push 
+#else
+ #   git push "${ORIGIN}" "${BRANCH}"
+#fi
